0a10a460ff9ad411bc2e0110e0a9eedf3b33a9c9
IvanMo76 <94475885+IvanMo76@users.noreply.github.com>
2026-07-18 00:52:49 -0600

    Update conex.py

--- conex.py
+++ conex.py
@@ -1,1 +1,12 @@
-DATABASE_URI = 'mysql+pymysql://root@localhost/financiera'
+import os
+
+DB_NAME = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE") or "railway"
+
+DATABASE_URI = (
+    f"mysql+pymysql://"
+    f"{os.getenv('MYSQLUSER')}:"
+    f"{os.getenv('MYSQLPASSWORD')}@"
+    f"{os.getenv('MYSQLHOST')}:"
+    f"{os.getenv('MYSQLPORT')}/"
+    f"{DB_NAME}"
+)
